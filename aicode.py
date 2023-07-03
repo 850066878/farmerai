4e6d58e0e79232a02e05a1e2c06dd0b16585debf
Reddy <850066878@genpact.com>
2023-07-03 17:52:52 +0530

    first commit

---
+++ aicode.py
@@ -0,0 +1,20 @@
+import openai
+import gradio
+
+openai.api_key = "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"
+
+messages = [{"role": "system", "content": "You are a financial experts that specializes in real estate investment and negotiation"}]
+
+def CustomChatGPT(user_input):
+    messages.append({"role": "user", "content": user_input})
+    response = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo",
+        messages = messages
+    )
+    ChatGPT_reply = response["choices"][0]["message"]["content"]
+    messages.append({"role": "assistant", "content": ChatGPT_reply})
+    return ChatGPT_reply
+
+demo = gradio.Interface(fn=CustomChatGPT, inputs = "text", outputs = "text", title = "")
+
+demo.launch(share=True)
